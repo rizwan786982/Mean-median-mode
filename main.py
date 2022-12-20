fdca444d42e00a76743625304bb779f50d3ba2a3
rizwan mohammed <>
2022-12-20 21:24:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+lst = [1,2,3,4,5,6,7,8,9,1]
+print (lst[0])
+mean = lst[0] + lst[1] + lst[2] + lst[3] + lst[4] + lst[5] + lst[6] + lst[7] + lst[8] + lst[9] 
+mean2 = mean / len(lst)
+print (mean2)
+
+lst = []
+rehanstotaliq = 0
+rehansiq = int(input("how many numbers do you want: "))
+for i in range (rehansiq):
+  rehansiq2 = int(input("Type in your numbers :"))
+  lst.append(rehansiq2)
+
+print(lst)
+
+for i in range (rehansiq):
+  rehanstotaliq = lst[i] + rehanstotaliq 
+      
+print(rehanstotaliq)  
+
+
+mode    =  
+if a ==1:
+  if b !=
+  if a   
